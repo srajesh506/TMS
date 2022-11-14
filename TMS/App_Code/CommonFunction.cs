2ba02f40f6de949d5136fa8c30d284c65cf87da6
Rajesh Shukla <106806480+srajesh506@users.noreply.github.com>
2022-11-14 16:11:00 +0530

    TMS Latest

---
+++ TMS/App_Code/CommonFunction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using TMS.App_Code;
+
+namespace TMS.App_Code
+{
+    class CommonFunction 
+    {
+        public string ConString=null;
+        public string ConstringServer=null;
+        public SqlConnection Con;
+        private SqlDataAdapter adp;
+        private SqlDataReader Dreader;
+        private SqlCommand cmd;
+        private SqlDataAdapter adapter;
+        private DataSet ds;
+        public CommonFunction()
+        {
+            try
+            {
+                LocalDatabaseCredential();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("please check Database Connection!! "+"'"+ex.Message+"'", "TMS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+
+        }
+        //Function to read AppConfig file for Connection String
+        public void LocalDatabaseCredential()
+        {
+            ConString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+            Con = new SqlConnection(ConString);
+            Con.Open();
+            Global.ConnectedServerName = Con.DataSource;
+            Global.connecteddatabasename = Con.Database;
+            Global.dbname = Global.connecteddatabasename;
+
+        }
+        //Check User Existence from UserDetail Table
+        public DataTable checkuserexistence(string username, string password)
+        {
+            if (Con.State == ConnectionState.Closed)
+            { Con.Open(); }
+            SqlCommand cmd;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            string selectcmd;
+            DataSet ds=new DataSet();
+            selectcmd = "Select RoleName,Roleid,Isadmin from Userdetails where userid='" + username + "' and password='" + password + "'";
+            cmd = new SqlCommand(selectcmd, Con);
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds);
+            return ds.Tables[0];
+        }
+        public void CloseConnection()
+        {
+            Con.Close();
+        }
+        public void OpenConnection()
+        {
+            if(Con.State==ConnectionState.Closed)
+            { Con.Open(); }
+        }
+            
+        
+    }
+}
---
+++ TMS/Home/TeamRegister.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMS.Home
+{
+    public partial class frmteamregister : Form
+    {
+        private Random random;
+        private int tempindex;
+        public frmteamregister()
+        {
+           
+            InitializeComponent();
+            random = new Random();
+            LoadTheme();
+        }
+
+        private void LoadTheme()
+        {
+           
+                foreach (Control btns in this.Controls)
+                {
+                    if (btns.GetType() == typeof(Button))
+                    {
+                    Button btn = (Button)btns;
+                    btn.BackColor = ThemeColor.PrimaryColor;
+                    btn.ForeColor = Color.White;
+                    btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                    }
+                }
+            btnadd.ForeColor = ThemeColor.PrimaryColor;
+            btnsave.ForeColor = ThemeColor.PrimaryColor;
+            btnmodify.ForeColor = ThemeColor.PrimaryColor;
+            btncancel.ForeColor = ThemeColor.PrimaryColor;
+            btnexit.ForeColor = ThemeColor.PrimaryColor;
+            lblreg.BackColor = ThemeColor.SecondaryColor;
+            lblempid.ForeColor = ThemeColor.SecondaryColor;
+            lblname.ForeColor = ThemeColor.SecondaryColor;
+            lblrole.ForeColor = ThemeColor.SecondaryColor;
+            lblemail.ForeColor = ThemeColor.SecondaryColor;
+            lblremark.ForeColor = ThemeColor.SecondaryColor;
+            groupBoxforregistration.ForeColor= ThemeColor.PrimaryColor;
+            groupBoxforbutton.ForeColor = ThemeColor.PrimaryColor;
+            groupBoxforexistingemployee.ForeColor = ThemeColor.PrimaryColor;
+            chkactive.ForeColor = ThemeColor.SecondaryColor;
+            txtempid.ForeColor= ThemeColor.SecondaryColor;
+            txtname.ForeColor = ThemeColor.SecondaryColor;
+            txtrole.ForeColor = ThemeColor.SecondaryColor;
+            txtemail.ForeColor = ThemeColor.SecondaryColor;
+            rtxtremark.ForeColor = ThemeColor.SecondaryColor;
+            txtpwd.ForeColor = ThemeColor.SecondaryColor;
+            btnupload.BackColor = ThemeColor.PrimaryColor;
+            btnupload.ForeColor = ThemeColor.SecondaryColor;
+            
+        }
+
+      
+
+        private Form activeForm;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dview_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnupload_Click(object sender, EventArgs e)
+        {
+            string imagelocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+                if(dialog.ShowDialog()==DialogResult.OK)
+                {
+                    imagelocation = dialog.FileName;
+                    pictureBox1.ImageLocation = imagelocation;
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("An error occured!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //private void OpenChildForm(Form childform, Object btnsender)
+        //{
+        //    if (activeForm != null)
+        //    {
+        //        activeForm.Close();
+        //    }
+        //    ActiveButton(btnsender);
+        //    activeForm = childform;
+        //    childform.TopLevel = false;
+        //    childform.FormBorderStyle = FormBorderStyle.None;
+        //    childform.Dock = DockStyle.Fill;
+        //    this.panel2.Controls.Add(childform);
+        //    this.panel2.Tag = childform;
+        //    childform.BringToFront();
+        //    childform.Show();
+        //    //labeltitle.Text = childform.Text;
+        //}
+        //private Button currentButton;
+        //private void ActiveButton(object btnsender)
+        //{
+        //    if (btnsender != null)
+        //    {
+        //        if (currentButton != (Button)btnsender)
+        //        {
+        //            DisableButton();
+        //            Color color = SelectThemeColor();
+        //            currentButton = (Button)btnsender;
+        //            currentButton.BackColor = color;
+        //            currentButton.ForeColor = Color.White;
+        //            currentButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        //            //paneltitlebar.BackColor = color;
+        //            //panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, 0.3);
+        //            ThemeColor.PrimaryColor = color;
+        //            ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, 0.3);
+        //            //btnclosechildform.Visible = true;
+        //        }
+        //    }
+        //}
+
+        //private Color SelectThemeColor()
+        //{
+        //    int index = random.Next(ThemeColor.ColorList.Count);
+        //    while (tempindex == index)
+        //    {
+        //        index = random.Next(ThemeColor.ColorList.Count);
+        //    }
+        //    tempindex = index;
+        //    string color = ThemeColor.ColorList[index];
+        //    return ColorTranslator.FromHtml(color);
+        //}
+        //private void DisableButton()
+        //{
+        //    foreach (Control previousbtn in panel2.Controls)
+        //    {
+        //        foreach (Control btn in previousbtn.Controls)
+        //        {
+        //            if (btn.GetType() == typeof(Button))
+        //            {
+        //                btn.BackColor = Color.FromArgb(35, 40, 45);
+        //                btn.ForeColor = Color.Gainsboro;
+        //                btn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        //            }
+
+        //        }
+        //    }
+        //}
+    }
+}
